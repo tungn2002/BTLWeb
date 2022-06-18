fb5a4d02bee4c3e53aeccf4fe3904a01e99de5ea
minhnam9apc <ngominhnam02@gmail.com>
2022-06-18 23:35:02 +0700

    xong giao dien dat ve

---
+++ assets/js/DatVe.js
@@ -0,0 +1,143 @@
+// Phần ngày tháng
+function myFunction12() {
+    choose();
+    document.getElementById("day12").style.border = "2px solid #222";
+}
+
+function myFunction13() {
+    choose();
+    document.getElementById("day13").style.border = "2px solid #222";
+}
+
+function myFunction14() {
+    choose();
+    document.getElementById("day14").style.border = "2px solid #222";
+}
+
+function myFunction15() {
+    choose();
+    document.getElementById("day15").style.border = "2px solid #222";
+}
+
+function myFunction16() {
+    choose();
+    document.getElementById("day16").style.border = "2px solid #222";
+}
+
+function myFunction17() {
+    choose();
+    document.getElementById("day17").style.border = "2px solid #222";
+}
+
+function myFunction18() {
+    choose();
+    document.getElementById("day18").style.border = "2px solid #222";
+}
+
+function myFunction19() {
+    choose();
+    document.getElementById("day19").style.border = "2px solid #222";
+}
+
+function myFunction_20() {
+    choose();
+    document.getElementById("day20").style.border = "2px solid #222";
+}
+
+function myFunction_21() {
+    choose();
+    document.getElementById("day21").style.border = "2px solid #222";
+}
+
+function myFunction_22() {
+    choose();
+    document.getElementById("day22").style.border = "2px solid #222";
+}
+
+function myFunction_23() {
+    choose();
+    document.getElementById("day23").style.border = "2px solid #222";
+}
+
+function choose() {
+    document.getElementById("day12").style.border = "0px solid #222";
+    document.getElementById("day13").style.border = "0px solid #222";
+    document.getElementById("day14").style.border = "0px solid #222";
+    document.getElementById("day15").style.border = "0px solid #222";
+    document.getElementById("day16").style.border = "0px solid #222";
+    document.getElementById("day17").style.border = "0px solid #222";
+    document.getElementById("day18").style.border = "0px solid #222";
+    document.getElementById("day19").style.border = "0px solid #222";
+    document.getElementById("day20").style.border = "0px solid #222";
+    document.getElementById("day21").style.border = "0px solid #222";
+    document.getElementById("day22").style.border = "0px solid #222";
+    document.getElementById("day23").style.border = "0px solid #222";
+}
+
+// Phần Thành Phố
+function myFunctionHCM() {
+    choose_place();
+    document.getElementById("HCM").style.backgroundColor = "black";
+    document.getElementById("HCM").style.color = "white";
+}
+
+function myFunctionHN() {
+    choose_place();
+    document.getElementById("HN").style.backgroundColor = "black";
+    document.getElementById("HN").style.color = "white";
+}
+
+function myFunctionDN() {
+    choose_place();
+    document.getElementById("DN").style.backgroundColor = "black";
+    document.getElementById("DN").style.color = "white";
+}
+
+function myFunctionCT() {
+    choose_place();
+    document.getElementById("CT").style.backgroundColor = "black";
+    document.getElementById("CT").style.color = "white";
+}
+
+function myFunctionDNai() {
+    choose_place();
+    document.getElementById("DNai").style.backgroundColor = "black";
+    document.getElementById("DNai").style.color = "white";
+}
+
+function myFunctionHP() {
+    choose_place();
+    document.getElementById("HP").style.backgroundColor = "black";
+    document.getElementById("HP").style.color = "white";
+}
+
+function myFunctionQN() {
+    choose_place();
+    document.getElementById("QN").style.backgroundColor = "black";
+    document.getElementById("QN").style.color = "white";
+}
+
+function myFunctionBR() {
+    choose_place();
+    document.getElementById("BR").style.backgroundColor = "black";
+    document.getElementById("BR").style.color = "white";
+}
+
+function choose_place() {
+    document.getElementById("HCM").style.backgroundColor = "white";
+    document.getElementById("HCM").style.color = "Black";
+    document.getElementById("HN").style.backgroundColor = "white";
+    document.getElementById("HN").style.color = "Black";
+    document.getElementById("DN").style.backgroundColor = "white";
+    document.getElementById("DN").style.color = "Black";
+    document.getElementById("CT").style.backgroundColor = "white";
+    document.getElementById("CT").style.color = "Black";
+    document.getElementById("DNai").style.backgroundColor = "white";
+    document.getElementById("DNai").style.color = "Black";
+    document.getElementById("HP").style.backgroundColor = "white";
+    document.getElementById("HP").style.color = "Black";
+    document.getElementById("QN").style.backgroundColor = "white";
+    document.getElementById("QN").style.color = "Black";
+    document.getElementById("BR").style.backgroundColor = "white";
+    document.getElementById("BR").style.color = "Black";
+}
