256c5b65831cc00e5f1f99af90657c6f00dc886f
Trunfg <buitruongar13@gmail.com>
2022-06-21 21:18:42 +0700

    BuiVanTruong

--- Chi_Tiet_Tai_Khoan/assets/js/script.js
+++ Chi_Tiet_Tai_Khoan/assets/js/script.js
@@ -2,26 +2,25 @@ function changePass(){
     let change = `
     <div>
         <label for="">Mật khẩu mới</label><br>
-        <input type="text" class="form_input">
+        <input type="text" class="form_input" id="passAfter">
     </div>
     <div>
         <label for="">Nhập lại mật khẩu mới</label><br>
-        <input type="text" class="form_input">
+        <input type="text" class="form_input" id="repass">
+    </div>
+    <div class="updoad">
+        <button type="submit" title="Lưu lại" class="luu" onclick="updoad()">Updoad</button>
     </div>
     `
     document.getElementById("add").innerHTML = change;
 }
-function save(){
-    let change = ``
-    document.getElementById("add").innerHTML  =change;
-}
+
 function load(){
-    let listUser = localStorage.getItem("list-user") ? JSON.parse(localStorage.getItem("list-user")):[];
-    let change = ` `
-    listUser.map((value,index)=>{
+    let list = localStorage.getItem("list-user") ? JSON.parse(localStorage.getItem("list-user")) : [];
+    let change = ``;
+    list.map((value, index)=>{
         if(value.login=='logging'){
-            change += `
-            <div class="page">
+            change = `<div class="page">
                 <h1>Thay đổi thông tin</h1>
             </div>
             <div class="c5_main">
@@ -29,17 +28,15 @@ function load(){
                     <form action="">
                         <div class="main_item">
                             <label for=""><strong>Họ và tên*</strong></label><br>
-                            <input type="text" value="${value.Name}" class="form_input"><br>
+                            <input type="text" value="${value.username}" class="name" id="Name"><br>
                         </div>
                         <div class="main_item">
                             <label for=""><strong>Điện thoại*</strong></label><br>
-                            <input type="text" value="${value.phonenumber}" class="form_input"><br>
+                            <input type="text" value="${value.phonenumber}" class="sdt" id="Sdt"><br>
                         </div>
                         <div class="main_item">
                             <label for=""><strong>Giới tính*</strong></label><br>
-                            <input type="radio" value="">nam
-                            <input type="radio" value="">nữ
-                            <input type="radio" value="">khác
+                            <input type="text" value="${value.gen}" id="gioitinh">
                             <br>
                         </div>
                         <div class="main_item">
@@ -48,7 +45,7 @@ function load(){
                         </div>
                         <div class="main_item">
                             <label for=""><strong>Địa chỉ email*</strong></label><br>
-                            ${email}
+                            ${value.email}
                         </div>
                     </form>
                 </div>
@@ -56,144 +53,81 @@ function load(){
                     <form action="">
                         <div class="main_item">
                             <label for=""><strong>Thành phố/Tỉnh*</strong></label><br>
-                            <select name="" id="" class="form_input">
-                                <option value="">Vui lòng chọn ... </option>
-                                <option value="">Hà Nội</option>
-                                <option value="">Hồ Chí Minh</option>
-                                <option value="">Đà Nẵng</option>
-                                <option value="">Hải Phòng</option>
-                                <option value="">Sơn La</option>
-                                <option value="">Bình Dương</option>
-                                <option value="">Thái Bình</option>
-                                <option value="">Thanh Hóa</option>
-                                <option value="">Thừa Thiên Huế</option>
-                                <option value="">Nghệ An</option>
-                            </select><br>
-                        </div>
-                        <div class="main_item">
-                            <label for=""><strong>Địa chỉ*</strong></label><br>
-                            <input type="text" value="${value.username}" class="form_input"><br>
-                        </div>
-                        <div class="main_item">
-                            <label for=""><strong>Mật khẩu cũ*</strong></label><br>
-                            <input type="text" value="" class="form_input"><br>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="c5_footer1">
-                <div class="c5_pass">
-                    <label for="" onclick="changePass()">Đổi mật khẩu</label>
-                </div>
-                
-                <div id="add"></div>
-            </div>
-            
-            <div class="c5_footer2">
-                <div class="first"><h2>Thông tin tùy chọn</h2></div>
-                <div class="list">
-                    <label for=""><strong>Rạp chiếu phim yêu thích</strong></label><br>
-                    <select name="" id="">
-                        <option value="">Vui lòng chọn ... </option>
-                        <option value="">CGV Aeon Long Biên</option>
-                        <option value="">CGV Aeon Hải phòng</option>
-                        <option value="">CGV Aeon Hà Đông</option>
-                        <option value="">CGV Aeon Hồ gươm plaza</option>
-                    </select>
-                </div>
-                <div class="save">
-                    <button type="submit" title="Lưu lại" class="button" onclick="save()">Lưu lại</button>
-                </div>
-            </div>
-            `
-        }
-        else{
-            change += `
-            <div class="page">
-                <h1>Thay đổi thông tin</h1>
-            </div>
-            <div class="c5_main">
-                <div class="c5_column">
-                    <form action="">
-                        <div class="main_item">
-                            <label for=""><strong>Họ và tên*</strong></label><br>
-                            <input type="text" value="" class="form_input"><br>
-                        </div>
-                        <div class="main_item">
-                            <label for=""><strong>Điện thoại*</strong></label><br>
-                            <input type="text" value="" class="form_input"><br>
-                        </div>
-                        <div class="main_item">
-                            <label for=""><strong>Giới tính*</strong></label><br>
-                            <input type="radio" value="">nam
-                            <input type="radio" value="">nữ
-                            <input type="radio" value="">khác
+                            <input type="text" class="tp" value="${value.khu}" id="tinh">
                             <br>
                         </div>
                         <div class="main_item">
-                            <label for="month"><strong>Ngày sinh*</strong></label><br>
-                        </div>
-                        <div class="main_item">
-                            <label for=""><strong>Địa chỉ email*</strong></label><br>
-                        </div>
-                    </form>
-                </div>
-                <div cass="c5_column">
-                    <form action="">
-                        <div class="main_item">
-                            <label for=""><strong>Thành phố/Tỉnh*</strong></label><br>
-                            <select name="" id="" class="form_input">
-                                <option value="">Vui lòng chọn ... </option>
-                                <option value="">Hà Nội</option>
-                                <option value="">Hồ Chí Minh</option>
-                                <option value="">Đà Nẵng</option>
-                                <option value="">Hải Phòng</option>
-                                <option value="">Sơn La</option>
-                                <option value="">Bình Dương</option>
-                                <option value="">Thái Bình</option>
-                                <option value="">Thanh Hóa</option>
-                                <option value="">Thừa Thiên Huế</option>
-                                <option value="">Nghệ An</option>
-                            </select><br>
-                        </div>
-                        <div class="main_item">
-                            <label for=""><strong>Địa chỉ*</strong></label><br>
-                            <input type="text" value="" class="form_input"><br>
-                        </div>
-                        <div class="main_item">
                             <label for=""><strong>Mật khẩu cũ*</strong></label><br>
-                            <input type="text" value="" class="form_input"><br>
+                            <input type="text" value="" class="mk" id="mkcu"><br>
                         </div>
                     </form>
                 </div>
             </div>
+
             <div class="c5_footer1">
                 <div class="c5_pass">
                     <label for="" onclick="changePass()">Đổi mật khẩu</label>
                 </div>
             
                 <div id="add"></div>
-                
             </div>
-        
+
             <div class="c5_footer2">
                 <div class="first"><h2>Thông tin tùy chọn</h2></div>
                 <div class="list">
                     <label for=""><strong>Rạp chiếu phim yêu thích</strong></label><br>
-                    <select name="" id="">
-                        <option value="">Vui lòng chọn ... </option>
-                        <option value="">CGV Aeon Long Biên</option>
-                        <option value="">CGV Aeon Hải phòng</option>
-                        <option value="">CGV Aeon Hà Đông</option>
-                        <option value="">CGV Aeon Hồ gươm plaza</option>
-                    </select>
+                    <input type="text" class="rap" value="${value.rap}" id="Rap">
                 </div>
                 <div class="save">
                     <button type="submit" title="Lưu lại" class="button" onclick="save()">Lưu lại</button>
                 </div>
-            </div>
-            `
+            </div>`;
+            document.getElementById("c5_22").innerHTML = change;
         }
-        document.getElementById("c5_22").innerHTML = change;
     })
+}
+function updoad(){
+    let list = localStorage.getItem("list-user") ? JSON.parse(localStorage.getItem("list-user")) : [];
+    let passafter = document.getElementById("passAfter").value;
+    let repass = document.getElementById("repass").value;
+    let mkCu = document.getElementById("mkcu").value;
+    list.map((value, index)=>{
+        if(value.login=='logging'){
+            if(mkCu==value.pass && passafter==repass){
+                value.pass = repass;
+                localStorage.setItem("list-user", JSON.stringify(list));
+                alert("Đổi mật khẩu thành công");
+            }
+            else{
+                alert("Mật khẩu không đúng");
+            }
+        }
+    })
+    let change = ``
+    document.getElementById("add").innerHTML = change;
+    load();
+}
+
+function save(){
+    let list = localStorage.getItem("list-user") ? JSON.parse(localStorage.getItem("list-user")) : [];
+    let name = document.getElementById("Name").value;
+    let sdt = document.getElementById("Sdt").value;
+    let gioitinh = document.getElementById("gioitinh").value;
+    let tinh = document.getElementById("tinh").value;
+    let rap = document.getElementById("Rap").value;
+    let pass = document.getElementById("mkcu").value;
+    list.map((value, index)=>{
+        if(value.login=='logging' && value.pass==pass){
+            value.username = name;
+            value.phonenumber = sdt;
+            value.gen = gioitinh;
+            value.khu = tinh;
+            value.rap = rap;
+            localStorage.setItem("list-user", JSON.stringify(list));
+        }
+        else{
+            alert("Mật khẩu không đúng");
+        }
+    })
+    load();
 }
--- Thong_Tin_Chung/assets/js/script.js
+++ Thong_Tin_Chung/assets/js/script.js
@@ -13,19 +13,51 @@ file.addEventListener('change', function(){
         });
         reader.readAsDataURL(chooseFile);
     }
-    const test = "./assets/img/" + this.files[0].name;
-    let list = localStorage.getItem("list-kh") ? JSON.parse(localStorage.getItem("list-kh")) : [];
-    list.push({
-        avatar: test
+    let list = localStorage.getItem("list-user") ? JSON.parse(localStorage.getItem("list-user")) : [];
+    list.map((value, index)=>{
+        if(value.login='logging'){
+            const test = "./assets/img/" + this.files[0].name;
+            value.avatar = test;
+            localStorage.setItem("list-user", JSON.stringify(list));
+        }
+        else{
+            alert("Bạn chưa đăng nhập");
+        }
     })
-    localStorage.setItem("list-kh", JSON.stringify(list));
+    loadding();
 });
 
 function loadding(){
-    let list = localStorage.getItem("list-kh") ? JSON.parse(localStorage.getItem("list-kh")) : [];
+    let list = localStorage.getItem("list-user") ? JSON.parse(localStorage.getItem("list-user")) : [];
     let change = ``;
+    let loadinfor = ``;
     list.map((value, index)=>{
-        change += `<img src="${value.avatar}" height="120" width="120" alt="AvatarProfile" class="img">`
+        if(value.login=='logging'){
+            if(value.avatar=='null'){
+                let change = `<img src="https://www.cgv.vn/skin/frontend/cgv/default/images/bg-cgv/icon-profile-cgv.png" height="120" width="120" alt="AvatarProfile" class="img">`;
+                loadinfor = `<p>Mã khách hàng: ${value.makh}</p>
+                <p>Họ và tên: ${value.username}</p>
+                <p>Email: ${value.email}</p>
+                <p>Số điện thoại: ${value.phonenumber}</p>`;
+                document.getElementById('loadAvatar').innerHTML=change;
+                document.getElementById('thongtin').innerHTML=loadinfor;
+            }
+            else{
+                change = `<img src="${value.avatar}" height="120" width="120" alt="AvatarProfile" class="img">`;
+                loadinfor = `<p>Mã khách hàng: ${value.makh}</p>
+                <p>Họ và tên: ${value.username}</p>
+                <p>Email: ${value.email}</p>
+                <p>Số điện thoại: ${value.phonenumber}</p>`;
+                document.getElementById('loadAvatar').innerHTML=change;
+                document.getElementById('thongtin').innerHTML=loadinfor;
+            }
+        }
+        else{
+            let change = `<img src="https://www.cgv.vn/skin/frontend/cgv/default/images/bg-cgv/icon-profile-cgv.png" height="120" width="120" alt="AvatarProfile" class="img">`;
+                loadinfor = ``;
+                document.getElementById('loadAvatar').innerHTML=change;
+                document.getElementById('thongtin').innerHTML=loadinfor;
+        }
+
     })
-    document.getElementById('loadAvatar').innerHTML=change;
 }
